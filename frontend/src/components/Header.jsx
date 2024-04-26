b42bd4e049a638b7a2f2f4931a31a180cb9c67a3
Zitan Salehin <zitan-74@dipti.com.bd>
2024-04-26 17:20:13 +0600

    add custom bootstrap theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+// import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MERN Shopping app</Navbar.Brand>
+          <Navbar.Brand href="/">
+            {/* <img src={logo} alt="logo" srcset="" /> */}
+            MERN Shopping app
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
