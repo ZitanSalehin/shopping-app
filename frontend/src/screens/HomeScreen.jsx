f7d3f3dc8f3f1fd1bdc6a5e0cc53519030e9f143
Zitan Salehin <zitan-74@dipti.com.bd>
2024-04-27 12:46:37 +0600

    product card

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>latest productes</h1>
+      <Row>
+        {products.map((product, index) => (
+          <Col key={index} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default HomeScreen;
